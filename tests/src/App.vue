0e294d267755b5bfa20f1fd7f4d6627516e74909
Sebastian Jimenez <Sebasjartiga@gmail.com>
2020-09-25 19:02:30 -0600

    Created new test project

---
+++ tests/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <p>{{mesage}}</p>
+    <input type="text" v-model="mesage">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      mesage:"holas"
+    };
+  },
+  components: {
+  }
+}
+</script>
